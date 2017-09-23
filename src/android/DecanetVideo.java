cf9f92b5638008c30efa714fb5af39522cc6e79a
Benjamin ARGOUD <benjamin@decanet.fr>
2017-09-23 19:50:11 +0200

    Path

--- src/android/DecanetVideo.java
+++ src/android/DecanetVideo.java
@@ -39,10 +39,10 @@ public class DecanetVideo extends CordovaPlugin {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        //FILE_PATH = Environment.getExternalStorageDirectory().toString() + "/";
+        FILE_PATH = Environment.getExternalStorageDirectory().toString() + "/";
         //FILE_PATH = cordova.getActivity().getCacheDir().toString() + "/";
 		//FILE_PATH = cordova.getActivity().getExternalCacheDir().toString() + "/";
-         FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
+        //FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
         //FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).toString() + "/";
     }
 
