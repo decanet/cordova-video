524f3c54310ae238bf460944e0646c5630a908ec
Benjamin ARGOUD <benjamin@decanet.fr>
2017-09-23 20:10:40 +0200

    path

--- src/android/DecanetVideo.java
+++ src/android/DecanetVideo.java
@@ -42,9 +42,9 @@ public class DecanetVideo extends CordovaPlugin {
         //FILE_PATH = Environment.getExternalStorageDirectory().toString() + "/";
         //FILE_PATH = cordova.getActivity().getCacheDir().toString() + "/";
 		//FILE_PATH = cordova.getActivity().getExternalCacheDir().toString() + "/";
-        //FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
+        FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
 		//FILE_PATH = cordova.getActivity().getExternalFilesDir().toString() + "/";
-        FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).toString() + "/";
+        //FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).toString() + "/";
     }
 
 
